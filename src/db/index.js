80b519df569ba557656fc1217744ea878bd5aa13
hdthuan <hdthuan.1990@gmail.com>
2022-06-09 12:50:24 +0700

    Allow custom firebase endpoint

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,19 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCptclDmlQPVgFFgbOOGV-FFaJWQK-fo2E",
+const getFirebaseConfigFromLocalStorage = () => {
+    if (!window.localStorage["firebaseConfig"]) return null;
+
+    return JSON.parse(window.localStorage["firebaseConfig"])
+}
+
+const firebaseConfig = getFirebaseConfigFromLocalStorage() || {
+    apiKey: "AIzaSyBLR5VQrhdmxocYP21Jsoi7jCWkBXhtxrk",
     authDomain: "ammtracker-1303c.firebaseapp.com",
     projectId: "ammtracker-1303c",
     storageBucket: "ammtracker-1303c.appspot.com",
     messagingSenderId: "736019759206",
-    appId: "1:736019759206:web:55e5f11ad4c63ed69aada7"
+    appId: "1:736019759206:web:c41bedd85d6191449aada7"
 };
 
 const app = initializeApp(firebaseConfig)
